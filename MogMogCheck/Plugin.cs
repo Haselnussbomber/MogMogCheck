5e35c1bb8b69207b0b871f2c8d0e8bbca63229d0
Haselnussbomber <mail@haselnussbomber.de>
2024-01-31 17:34:48 +0100

    remove resolve call

--- MogMogCheck/Plugin.cs
+++ MogMogCheck/Plugin.cs
@@ -13,20 +13,13 @@ public sealed class Plugin : IDalamudPlugin
         Service.Initialize(pluginInterface);
         Service.AddService(Configuration.Load());
 
-        CommandInfo = new CommandInfo(OnCommand) { HelpMessage = t("CommandHandlerHelpMessage") };
-
-        Service.Framework.RunOnFrameworkThread(Setup);
-    }
-
-    private void Setup()
-    {
-        HaselCommon.Interop.Resolver.GetInstance.Resolve();
-
         Service.PluginInterface.LanguageChanged += PluginInterface_LanguageChanged;
         Service.PluginInterface.UiBuilder.OpenMainUi += OpenMainUi;
         Service.PluginInterface.UiBuilder.OpenConfigUi += OpenConfigUi;
         Service.AddonObserver.AddonOpen += AddonObserver_AddonOpen;
         Service.AddonObserver.AddonClose += AddonObserver_AddonClose;
+
+        CommandInfo = new CommandInfo(OnCommand) { HelpMessage = t("CommandHandlerHelpMessage") };
 
         Service.CommandManager.AddHandler("/mogmog", CommandInfo);
     }
