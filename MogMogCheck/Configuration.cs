6b33d2bd239cb86d5a48f27d53dcb7d331d08a57
Haselnussbomber <mail@haselnussbomber.de>
2024-03-10 03:52:50 +0100

    update HaselCommon

--- MogMogCheck/Configuration.cs
+++ MogMogCheck/Configuration.cs
@@ -6,7 +6,6 @@ using System.Text.Json.Serialization;
 using Dalamud.Configuration;
 using Dalamud.Interface.Internal.Notifications;
 using Dalamud.Utility;
-using HaselCommon.Extensions;
 
 namespace MogMogCheck;
 
@@ -35,7 +34,7 @@ public partial class Configuration : IDisposable
     };
 
     [JsonIgnore]
-    public string LastSavedConfigHash = string.Empty;
+    public int LastSavedConfigHash;
 
     public static Configuration Load()
     {
@@ -121,7 +120,7 @@ public partial class Configuration : IDisposable
         try
         {
             var serialized = JsonSerializer.Serialize(this, DefaultJsonSerializerOptions);
-            var hash = serialized.GetHash();
+            var hash = serialized.GetHashCode();
 
             if (LastSavedConfigHash != hash)
             {
